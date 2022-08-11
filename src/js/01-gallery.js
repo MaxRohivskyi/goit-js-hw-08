4c6c7cbc2f1d57cfc85d7e9f882963b4cd6be558
Max Rohivskyi <maxrogivsky@gmail.com>
2022-08-11 16:18:54 +0300

    add task 1 image-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,43 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Module with Babel tranformation
+import SimpleLightbox from "simplelightbox";
+
+// Plain ES Module without Babel
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// 1. Створення і рендер розмітки на підставі масиву даних galleryItems
+// і наданого шаблону елемента галереї.
+
+const galleryRef = document.querySelector('.gallery');
+
+const addGalleryMarkup = galleryItems.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+        <img 
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" 
+        />        
+    </a>`
+}).join('')
+
+galleryRef.innerHTML = addGalleryMarkup;
+
+// 3. Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
+
+let gallery = new SimpleLightbox('.gallery a', {
+
+    // 4. Відображення підписів до зображень з атрибута alt.
+    // Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+
+    captionsData: "alt",
+    captionDelay: '250ms'
+});
+
+gallery.on('show.simplelightbox');
+
+// console.log(galleryItems);
