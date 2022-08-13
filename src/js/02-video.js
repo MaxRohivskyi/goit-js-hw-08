71b05395e2745a8dd34d9c01211e00eb67ebe64b
Max Rohivskyi <maxrogivsky@gmail.com>
2022-08-14 01:35:23 +0300

    add task 2 video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let throttle = require('lodash.throttle');
 
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay), 1000);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {
+  // seconds = the actual time that the player seeked to
+  console.log(seconds);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
